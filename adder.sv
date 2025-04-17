7d534b7d97f82bb787f89ee5e8f7f7a7f9ba55be
KennethC888 <157078933+KennethC888@users.noreply.github.com>
2025-04-17 15:16:46 -0400

    Create adder.sv

---
+++ adder.sv
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Module: Adder
+//
+// n-bit Full Adder using Behavioral Modeling
+//
+// module: FullAdder
+// hdl: SystemVerilog
+// modeling: Behavioral Modeling
+///////////////////////////////////////////////////////////////////////////////
+`ifndef ADDER
+`define ADDER
+
+module ADDER #(
+  parameter WIDTH = 32 // Default width of 32 bits
+) (
+  input logic [WIDTH-1:0] A, // Input A
+  input logic [WIDTH-1:0] B, // Input B
+  output logic [WIDTH-1:0] Sum, // Sum output
+);
+
+  // Behavioral modeling for the n-bit adder
+    assign Sum = A + B; // Perform addition 
+
+endmodule
+`endif
