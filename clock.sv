1773a3a4713b3aa7a7ea35981ad90b2da7c9dd44
KennethC888 <157078933+KennethC888@users.noreply.github.com>
2025-04-17 14:49:14 -0400

    Create clock.sv

---
+++ clock.sv
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// Module: CLOCK
+//
+// CLOCK
+//
+// module: CLOCK
+// hdl: SystemVerilog
+// modeling: Behavior Level Modeling
+///////////////////////////////////////////////////////////////////////////////
+`ifndef CLOCK
+`define CLOCK
+// DO NOT FORGET TO RENAME MODULE_NAME to match your module_name
+
+module CLOCK(clk
+ );
+    //
+    // ---------------- DECLARATIONS OF PORT IN/OUT & DATA TYPES ----------------
+    //
+    input logic clk; 
+
+    
+endmodule
+
+`endif // CLOCK
