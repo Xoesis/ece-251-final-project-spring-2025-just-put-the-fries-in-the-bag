6f6c40e287cf4e1eed25a0b83facda3e64eaa421
KennethC888 <157078933+KennethC888@users.noreply.github.com>
2025-04-17 15:17:53 -0400

    Create tb_adder.sv

---
+++ tb_adder.sv
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Module: Testbench for Adder
+//
+// Testbench for Adder
+//
+// module: Adder
+// hdl: SystemVerilog
+///////////////////////////////////////////////////////////////////////////////
+`ifndef TB_ADDER
+`define TB_ADDER
+
+`timescale 1ns/100ps
+`include "adder.sv"
+
+module tb_adder;
+    parameter n = 32;
+    logic [(n-1):0] a, b, y;
+
+   initial begin
+        $dumpfile("adder.vcd");
+        $dumpvars(0, uut);
+        $monitor("a = 0x%0h b = 0x%0h y = 0x%0h", a, b, y);
+    end
+
+    initial begin
+        a <= #n'hFFFFFFFF;
+        b <= #n'hFFFFFFFF;
+    end
+
+    adder uut(
+        .A(a), .B(b), .Y(y)
+    );
+endmodule
+`endif // TB_ADDER
