5f15bbf2eb9c827255eebbda4d02fb2f1082276f
KennethC888 <157078933+KennethC888@users.noreply.github.com>
2025-05-10 03:08:38 -0400

    Create tb_IMEM.sv

---
+++ tb_IMEM.sv
@@ -0,0 +1,45 @@
+//////////////////////////////////////////////////////////////////////////////////
+// The Cooper Union
+// ECE 251 Spring 2023
+// Code inspired by Prof. Rob Marano 
+// Kenneth Chan 
+// 
+// 
+//     Module Name: tb_IMEM
+//     Description: Test bench 
+//
+//
+//////////////////////////////////////////////////////////////////////////////////
+`ifndef TB_IMEM
+`define TB_IMEM
+
+`timescale 1ns/100ps
+`include "IMEM.sv"
+
+module tb_IMEM;
+    parameter n = 32; // bit length of registers/memory
+    parameter r = 6; // we are only addressing 64=2**6 mem slots in imem
+    logic [(n-1):0] readdata;
+    logic [(r-1):0] imem_addr;
+
+   initial begin
+        $dumpfile("imem.vcd");
+        $dumpvars(0, uut);
+        //$monitor("enable = %b clk = %b", enable, clk);
+        $monitor("time=%0t \t imem_addr=%b readdata=%h",$realtime, imem_addr, readdata);
+    end
+
+    initial begin
+        #10 imem_addr <= #(r)'b000000;
+        #10 imem_addr <= #(r)'b000001;
+        #10 imem_addr <= #(r)'b000010;
+        $finish;
+    end
+
+   IMEM uut(
+        .addr(imem_addr),
+        .readdata(readdata)
+    );
+endmodule
+
+`endif // TB_IMEM
