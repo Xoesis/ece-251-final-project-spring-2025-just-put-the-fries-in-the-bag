af7a343dd4248125019eafaf6ced392748aafd22
KennethC888 <157078933+KennethC888@users.noreply.github.com>
2025-05-10 03:04:17 -0400

    Update clock.sv

--- clock.sv
+++ clock.sv
@@ -10,16 +10,47 @@
 ///////////////////////////////////////////////////////////////////////////////
 `ifndef CLOCK
 `define CLOCK
-// DO NOT FORGET TO RENAME MODULE_NAME to match your module_name
 
-module CLOCK(clk
- );
-    //
-    // ---------------- DECLARATIONS OF PORT IN/OUT & DATA TYPES ----------------
-    //
-    input logic clk; 
+`timescale 1ns/100ps
 
-    
+module CLOCK
+    #(parameter ticks = 10)(
+    //
+    // ---------------- PORT DEFINITIONS ----------------
+    //
+    input ENABLE,
+    output reg CLOCK
+);
+    //
+    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
+    //
+    reg start_clock;
+    real clock_on = ticks/2; // duty cycle = 50%
+    real clock_off = ticks/2;
+
+    // initialize variables
+    initial begin
+      CLOCK <= 0;
+      start_clock <= 0;
+    end
+
+    always @(posedge ENABLE or negedge ENABLE) begin
+        if (ENABLE) begin
+            start_clock = 1;
+        end
+        else begin
+            start_clock = 0;
+        end
+        // #ticks CLOCK = ~CLOCK;
+    end
+    always @(start_clock) begin
+        CLOCK = 0;
+        while (start_clock) begin
+            #(clock_off) CLOCK = 1;
+            #(clock_on) CLOCK = 0;
+        end
+        CLOCK = 0;
+    end
 endmodule
 
 `endif // CLOCK
